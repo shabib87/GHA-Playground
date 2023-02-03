d427fe3a178f1e3579de452dda3445438bd2d83c
Ahmad Shabibul Hossain <ahmad.hossain@vts.com>
2023-02-03 12:50:59 -0500

    release workflow started

---
+++ version_validation.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+version = ARGV[0]
+
+if version =~ regex
+    puts "Valid version number"
+else
+    puts "Invalid version number"
+    exit 1
+end
